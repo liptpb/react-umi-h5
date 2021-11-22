7fc6a159fe9da9f92747a6907c193715d2f3ba5f
liboqian <liboqian@drumbeatsoft.com>
2021-11-22 17:31:52 +0800

    11

---
+++ src/layouts/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+interface AppProps {
+  value?: string;
+}
+const LayIndex: React.FC<AppProps> = (props: AppProps)=> {
+  return (
+    <div className=''>
+      111
+    </div>
+  );
+};
+export default LayIndex;
